72d2d537a31797147f2fccef7808760c5b651bee
Rafael S. Bittencourt <154441296+faelvulgo@users.noreply.github.com>
2024-05-17 12:02:53 -0300

    Update dataplotter.py

--- Plotters/Misc/dataplotter.py
+++ Plotters/Misc/dataplotter.py
@@ -14,11 +14,11 @@ def plot_temperature_over_time(input_files):
 
         # Plot temperature over time for the current file using a different color
         ctd_df = ctd_data.copy()  # Make a copy to avoid modifying the original DataFrame
-        ctd_df['// timestamp(yyyy-mm-ddTHH:MM:ss.FFF)'] = pd.to_datetime(
-            ctd_df['// timestamp(yyyy-mm-ddTHH:MM:ss.FFF)'])
+        ctd_df[''] = pd.to_datetime(
+            ctd_df[''])
         print(f'Plotting data from {input_file}...')
-        plt.plot(ctd_df['// timestamp(yyyy-mm-ddTHH:MM:ss.FFF)'],
-                 ctd_df['temperature(°C)'],
+        plt.plot(ctd_df[''],
+                 ctd_df[''],
                  label=os.path.basename(input_file),  # Use the file name as label
                  color=colors[i % len(colors)])  # Cycle through colors list
 
@@ -33,12 +33,7 @@ def plot_temperature_over_time(input_files):
 
 
 if __name__ == "__main__":
-    input_files = [r"D:\InternalWaves_teste\cruzeiro2\200423_20200228_1645_light.csv",
-                   r"D:\InternalWaves_teste\cruzeiro2\200424_20200228_1608_light.csv",
-                   r"D:\InternalWaves_teste\cruzeiro2\200425_20200228_1551_light.csv",
-                   r"D:\InternalWaves_teste\cruzeiro2\200426_20200228_1527_light.csv",
-                   r"D:\InternalWaves_teste\cruzeiro2\200427_20200228_1501_light.csv",
-                   r"D:\InternalWaves_teste\cruzeiro2\200428_20200227_2329_light.csv"]
-    save_path = r"D:\InternalWaves_teste\cruzeiro2"
+    input_files = [r""]
+    save_path = r""
 
     plot_temperature_over_time(input_files)
